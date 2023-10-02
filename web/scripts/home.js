dbbdaf65a4688dfc76305e7f15d2ff5b75fc9b62
Arindam Nag <arindamnag54321@gmail.com>
2023-10-02 22:46:56 +0530

    Added support for dummy data

--- web/scripts/home.js
+++ web/scripts/home.js
@@ -1,32 +1,35 @@
 async function getUpdates() {
-    return fetch('https://api.cricapi.com/v1/currentMatches?apikey=d331eb69-7b42-4a46-bf78-e96b240f046e&offset=0')
-        .then(data => data.json())
-        .then(data => {
+    // return fetch('https://api.cricapi.com/v1/currentMatches?apikey=d331eb69-7b42-4a46-bf78-e96b240f046e&offset=0')
+    //     .then(data => data.json())
+    return fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then((data) => dummy_data)
+        .then((data) => {
             if (data.status != 'success') {
                 return
             }
-            
+
             const matches = data.data
             if (!matches) {
                 return []
             }
 
             const relevantdata = matches.map(
-                match => `<td class="match-name">${match.name}: </td><td class="match-status">${match.status}</td>`
+                (match) =>
+                    `<td class="match-name">${match.name}: </td><td class="match-status">${match.status}</td>`
             )
-            
+
             console.log(relevantdata)
 
             document.getElementById('match-table').innerHTML =
                 `
-                    <table id = "match-table"  class="table table-bordered table-dashed">
+                    <table id = "match-table"  class="table table-bordered table-dashed table-hover">
                         <thead>
                             <th>Name</th>
                             <th>Status</th>
                         </thead>
                         <tbody>
                 ` +
-                        relevantdata.map((match) => `<tr>${match}</tr>`).join('') +
+                relevantdata.map((match) => `<tr>${match}</tr>`).join('') +
                 `
                         </tbody>
                     </table>
